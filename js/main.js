e3a0328622276c6807a42671374d284ffefd3244
Luke Lawton <llawton25@student.dimanregional.org>
2023-05-25 20:17:03 -0400

    jump

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function roundDownToMultiple(num, mult) {
 } 
 
 let game = {
-    blocksWide: 30
+    blocksWide: 35
 }
 
 startGame()
@@ -16,11 +16,16 @@ function startGame() {
     setCanvasSize()
     
     let frame = setInterval(() => {
-        gravity()
-        characterMove()
+        characterGravity()
+        characterController()
         characterDrag()
+        characterJump()
+        
+        characterBorderCheck()
         characterPositionCalculation()
     
+        console.clear()
+        console.log(game.character.speedX, game.character.speedY)
         drawFrame()
     }, 1 / 60);
 }
@@ -48,10 +53,24 @@ game = {
         walkAcceleration: 3,
         sprintSpeed: 15,  // max speed
         sprintAcceleration: 6,
-        drag: 0.5, // constant force dragging the speed down
-        gravAcceleration: 1, // how fast gravity accelerates
-        gravSpeed: 0, // current gravity speed
+        airDrag: 0.1, // constant force dragging the speed down while in the air
+        walkingDrag: 0.5, // constant force dragging the speed down while grounded
+        gravAcceleration: 0.5, // how fast gravity accelerates
         direction: 1,
+        grounded: false,
+
+        // landing bounce is when you land from a fall and hold jump while you land
+        landBounceHeightMult: 0.2,
+        landBounceSpeedBoost: 2,
+        landBounceMinSpeed: 20,
+
+        // jump
+        jumpTick: 0,
+        jumpDuration: 10, // in frames
+        initialJumpYBoost: 3,
+        initialJumpXBoost: 1.7,
+        jumpHoldYBoost: 1.1,
+
         controls: {
             leftBtn: [
                 'a',
@@ -69,8 +88,8 @@ game = {
                 's',
                 'ArrowDown'
             ],
-            sprintBtn: [
-                'Shift',
+            jumpBtn: [
+                ' ',
                 'null'
             ]
         },
@@ -79,31 +98,70 @@ game = {
             right: false,
             up: false,
             down: false,
-            sprint: false
+            jump: false,
         }
     },
-    blockSize: canvas.width / 30,
+    blockSize: canvas.width / game.blocksWide,
     blocksWide: game.blocksWide,
     backgroundColor: '#87CEEB'
 }
 
-function gravity() {
-
-}
-
-
-function characterDrag() {
-    if (game.character.speedX > 0) {
-        if (game.character.speedX - game.character.drag < 0) game.character.speedX = 0 // helps with weird numbers of drag
-        game.character.speedX -= game.character.drag
-    } else if (game.character.speedX < 0) {
-        if (game.character.speedX + game.character.drag > 0) game.character.speedX = 0 // helps with weird numbers of drag
-        game.character.speedX += game.character.drag
+function characterJump() {
+    if (game.character.heldButtons.jump && game.character.jumpTick < game.character.jumpDuration) {
+        console.log('red')
+        if (game.character.jumpTick == 0) { // on the first jump frame the player gets a boost
+            game.character.grounded = false
+            game.character.speedY -= game.character.initialJumpYBoost
+            game.character.speedX *= game.character.initialJumpXBoost
+        }
+        game.character.jumpTick++
+        game.character.speedY -= game.character.jumpHoldYBoost
     }
 }
 
+function characterBorderCheck() {
+    if (game.character.y + game.character.height > canvas.height) {
+        // landing
+        if (game.character.heldButtons.jump && game.character.speedY > game.character.landBounceMinSpeed) {
+            game.character.y = canvas.height - (game.character.height * game.blockSize)
+            game.character.speedY = -game.character.speedY * game.character.landBounceHeightMult
+            game.character.speedX *= game.character.landBounceSpeedBoost
+            return
+        }
 
-function characterMove() {
+        game.character.speedY = 0
+        game.character.grounded = true
+        game.character.jumpTick = 0 // allows the player to jump again
+        game.character.y = canvas.height - (game.character.height * game.blockSize)
+    }
+}
+
+function characterGravity() {
+    if (game.character.grounded) return
+    game.character.speedY += game.character.gravAcceleration
+}
+
+function characterDrag() {
+    if (game.character.speedX > 0) {
+        if (game.character.speedX - game.character.walkingDrag < 0) game.character.speedX = 0 // helps with weird numbers of drag
+        
+        if (game.character.grounded) { // drag only occurs while grounded
+            game.character.speedX -= game.character.walkingDrag
+        } else {
+            game.character.speedX -= game.character.airDrag
+        }
+    } else if (game.character.speedX < 0) {
+        if (game.character.speedX + game.character.walkingDrag > 0) game.character.speedX = 0 // helps with weird numbers of drag
+
+        if (game.character.grounded) { // drag only occurs while grounded
+            game.character.speedX += game.character.walkingDrag
+        } else {
+            game.character.speedX += game.character.airDrag
+        }
+    }
+}
+
+function characterController() {
     if (game.character.heldButtons.sprint && game.character.heldButtons.right) {
         // stops from accelerating past max speed
         if (game.character.speedX + game.character.sprintAcceleration <= game.character.sprintSpeed) {
@@ -135,6 +193,7 @@ function characterMove() {
 
 function characterPositionCalculation() {
     game.character.x += game.character.speedX
+    game.character.y += game.character.speedY
 }
 
 function drawBackground() {
@@ -171,9 +230,9 @@ window.addEventListener('keydown', e => {
     if (e.key == game.character.controls.downBtn[0] || e.key == game.character.controls.downBtn[1]) {
         game.character.heldButtons.down = true
     } 
-    
-    if (e.key == game.character.controls.sprintBtn[0] || e.key == game.character.controls.sprintBtn[1]) {
-        game.character.heldButtons.sprint = true
+
+    if (e.key == game.character.controls.jumpBtn[0] || e.key == game.character.controls.jumpBtn[1]) {
+        game.character.heldButtons.jump = true
     }
 })
 
@@ -193,8 +252,8 @@ window.addEventListener('keyup', e => {
     if (e.key == game.character.controls.downBtn[0] || e.key == game.character.controls.downBtn[1]) {
         game.character.heldButtons.down = false
     } 
-    
-    if (e.key == game.character.controls.sprintBtn[0] || e.key == game.character.controls.sprintBtn[1]) {
-        game.character.heldButtons.sprint = false
+
+    if (e.key == game.character.controls.jumpBtn[0] || e.key == game.character.controls.jumpBtn[1]) {
+        game.character.heldButtons.jump = false
     }
 })
